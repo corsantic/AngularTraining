9fde5c2bd32631318277394571d07fd8a2ccb5aa
corsantic <aydofurkan25@gmail.com>
2017-11-14 16:07:53 +0300

    async validate but not working right now

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -8,9 +8,24 @@ export class UsernameValidators {
     static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
 
         if ((control.value as string).indexOf(' ') >= 0) {
-            return { cannotContainSpace:true };
+            return { cannotContainSpace: true };
         }
         return null;
+
+    }
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'furkan')
+                    resolve({ shouldBeUnique: true });
+                else
+                        resolve(null);
+
+            }, 1000);
+        });
+
+
 
     }
 
