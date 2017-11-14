b887a9b998ff334a80b90afb1adbe6f34ebd7245
corsantic <aydofurkan25@gmail.com>
2017-11-14 10:44:12 +0300

    remove Course and add course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,17 @@ courses=[
   { id:2,name:'course2'},
   { id:3,name:'course3'}
 ];
+onAdd()
+{
+  this.courses.push({
+    id:4,name:'course4'
+  });
+}
+onChange(course){
+ let index= this.courses.indexOf(course);
+ this.courses.splice(index,1);
+ 
+
+}
 
 }
