e94d978a4e0d957cf5196820bdbf55fd23973e15
corsantic <aydofurkan25@gmail.com>
2017-11-15 13:23:13 +0300

    call ngOnInit for initialize not constructor

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,49 +7,17 @@ import { Http } from '@angular/http';
   styleUrls: ['./posts.component.css'],
 
 })
-export class PostsComponent {
-    4
+export class PostsComponent implements OnInit{
   posts: any[];
   private url = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: Http) {
-    http.get(this.url)
-      .subscribe(response => {
-        this.posts = (response.json());
-      });
   }
 
-  createPost(input: HTMLInputElement) {
-    let post: any = {
-      title: input.value
-    };
-    input.value = '';
-    this.http.post(this.url, JSON.stringify(post))
-      .subscribe(response => {
-
-        post.id = response.json().id;
-        this.posts.splice(0, 0, post);
-      });
-  }
-  //patch post.id post.title
-  //post post
-  updatePost(post){
-    this.http.patch(this.url+'/'+post.id,JSON.stringify({isRead:true}))
-    .subscribe(response=>
-    {
-      console.log(response.json());
-
-    });
-    //this.http.put(this.url,JSON.stringify(post))
-  }
-
-  deletePost(post)
-{
-  this.http.delete(this.url+'/'+post.id)
-  .subscribe(response=>{
-    let index=this.posts.indexOf(post);
-    this.posts.splice(index,1);
-  });
-}
-
+  ngOnInit() {
+    this.http.get(this.url)
+     .subscribe(response => {
+       this.posts = (response.json());
+     });
+   }
 
 }
