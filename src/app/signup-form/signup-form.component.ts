664555d1c676f39a4a3d65173dbac1825dbf952a
corsantic <aydofurkan25@gmail.com>
2017-11-15 09:21:32 +0300

    login errors

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,28 +1,36 @@
 import { UsernameValidators } from './username.validators';
 import { Component } from '@angular/core';
-import{FormGroup,FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
-        form=new FormGroup({
-    username:new FormControl('',[
-      Validators.required,  
-       UsernameValidators.cannotContainSpace,
-      Validators.minLength(3)] , UsernameValidators.shouldBeUnique
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      UsernameValidators.cannotContainSpace,
+      Validators.minLength(3)], UsernameValidators.shouldBeUnique
 
     ),
-    password:new FormControl('',Validators.required)
+    password: new FormControl('', Validators.required)
   });
-  
 
-  get username(){
+  login() {
+    this.form.setErrors({
+      invalidLogin: true
+    });
+
+  }
+
+
+
+  get username() {
     return this.form.get('username');
   }
-get password(){
-  return this.form.get('password');
-}
+  get password() {
+    return this.form.get('password');
+  }
 
 }
