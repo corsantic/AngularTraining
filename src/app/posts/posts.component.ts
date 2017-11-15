154f50fc489e50a41f23ad699c704cfc53db2d0c
corsantic <aydofurkan25@gmail.com>
2017-11-15 12:55:58 +0300

    update data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,31 +7,40 @@ import { Http } from '@angular/http';
   styleUrls: ['./posts.component.css'],
 
 })
-export class PostsComponent {4
-  posts:any[];
-  private url='https://jsonplaceholder.typicode.com/posts';
-  constructor(private http:Http) {
+export class PostsComponent {
+    4
+  posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: Http) {
     http.get(this.url)
-    .subscribe(response=>{
-      this.posts=(response.json());
+      .subscribe(response => {
+        this.posts = (response.json());
+      });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post: any = {
+      title: input.value
+    };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+
+        post.id = response.json().id;
+        this.posts.splice(0, 0, post);
+      });
+  }
+  //patch post.id post.title
+  //post post
+  updatePost(post){
+    this.http.patch(this.url+'/'+post.id,JSON.stringify({isRead:true}))
+    .subscribe(response=>
+    {
+      console.log(response.json());
+
     });
-   }
-
-   createPost(input:HTMLInputElement)
-   {
-     let post:any={
-       title:input.value
-     };
-     input.value='';
-     this.http.post(this.url,JSON.stringify(post))
-     .subscribe(response=>{
-    
-      post.id=response.json().id;
-      this.posts.splice(0,0,post);
-     });
-     
-
- }
+    //this.http.put(this.url,JSON.stringify(post))
+  }
 
 
 
