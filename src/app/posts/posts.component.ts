3240b3ad610a675548c6e8291b6d0e2997a040ac
corsantic <aydofurkan25@gmail.com>
2017-11-16 09:10:51 +0300

    optimistic solution

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -27,15 +27,15 @@ export class PostsComponent implements OnInit {
       title: input.value
     };
     this.posts.splice(0, 0, post);
-    
+
     input.value = '';
     this.service.create(post)
       .subscribe(newPost => {
-        post.id =newPost.id;
-       
+        post.id = newPost.id;
+
       },
       (error: AppError) => {
-        this.posts.splice(0,1);
+        this.posts.splice(0, 1);
 
         if (error instanceof BadInput) {
           alert("This post has already created");
@@ -60,12 +60,15 @@ export class PostsComponent implements OnInit {
   }
 
   deletePost(post) {
-    this.service.delete(305)
-      .subscribe(() => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      },
+
+    let index = this.posts.indexOf(post);
+    this.posts.splice(index, 1);
+
+    this.service.delete(post.id)
+      .subscribe(
+        null,
       (error: AppError) => {
+        this.posts.splice(index, 0, post);
         if (error instanceof NotFoundError)
           alert("This post has already been deleted");
         else throw error;
